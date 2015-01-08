74bee7370787bba82b1acf2c2bdc1d1b743fd66b
Chris Duncan <cjdstormy@gmail.com>
2015-01-08 02:19:41 -0500

    Create GamePanel.java

---
+++ GamePanel.java
@@ -0,0 +1,21 @@
+
+/* Description: Improves upon the Panel built into Java
+ * Author: Chris Duncan
+ * Date Modified: 1/6/15
+ * Version: 1.6
+ * Copyright 2015, Chris Duncan, All rights reserved.
+ *
+ * If you need backround information, be sure to read the readme.txt included with the files necessary to run the project
+*/
+
+import java.awt.*;
+
+public class GamePanel extends Panel {	
+	
+	private static final long serialVersionUID = 1L;
+
+	public GamePanel(Dimension d) {
+       super();
+       this.setPreferredSize(d);
+    }
+}
